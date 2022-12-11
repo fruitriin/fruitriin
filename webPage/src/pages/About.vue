3d3a21e5f9629cf645d142ac6ef00221b715f605
FruitRiin <nassii74@gmail.com>
2022-12-11 11:53:58 +0900

    build忘れ他

--- webPage/src/pages/About.vue
+++ webPage/src/pages/About.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import titleLogo from '../assets/titleLogo.png'
 </script>
 <template>
--- webPage/src/pages/missRirica/term.vue
+++ webPage/src/pages/missRirica/term.vue
@@ -1,4 +1,3 @@
-<script setup></script>
 
 <template>
   <strong>Terms &amp; Conditions</strong> <p>
