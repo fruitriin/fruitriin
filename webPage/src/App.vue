e1f6294e506c69afc83a511dbe98e17a58b868bb
FruitRiin <nassii74@gmail.com>
2022-12-11 11:32:53 +0900

    VueRouter対応 MissRiricaプラポリ対応

--- webPage/src/App.vue
+++ webPage/src/App.vue
@@ -1,26 +1,7 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import titleLogo from './assets/titleLogo.png'
-</script>
 
 <template>
   <div>
-    <a href="/" >
-      <img :src="titleLogo" class="logo" alt="Riin's Workspace" />
-    </a>
+    <router-view></router-view>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ webPage/src/pages/About.vue
@@ -0,0 +1,21 @@
+<script setup>
+import titleLogo from '../assets/titleLogo.png'
+</script>
+<template>
+  <a href="/" >
+    <img :src="titleLogo" class="logo" alt="Riin's Workspace" />
+  </a>
+</template>
+
+<style>
+.logo {
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
