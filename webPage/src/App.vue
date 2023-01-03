c540b01f227ee828bb7a80c02ae27709ddc25a29
FruitRiin <nassii74@gmail.com>
2023-01-03 17:19:06 +0900

    トップページを正しく中央揃えになるよう修正

--- webPage/src/App.vue
+++ webPage/src/App.vue
@@ -1,7 +1,15 @@
 
 <template>
-  <div>
     <router-view></router-view>
-  </div>
 </template>
 
+<style>
+
+html, body,#app{
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
