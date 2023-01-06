5931c452865444c080a36e50fc3a6a6deca40db0
FruitRiin <nassii74@gmail.com>
2023-01-06 22:22:34 +0900

    build

--- webPage/nuxt.config.ts
+++ webPage/nuxt.config.ts
@@ -1,47 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  head: {
-    meta: [
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      {
-        charset: "utf-8",
-      },
-      {
-        property: "og:title",
-        content: "Riin's Workspace",
-      },
-      {
-        property: "og:description",
-        content: "Vueが好きな人",
-      },
-      {
-        property: "og:type",
-        content: "website",
-      },
-      {
-        property: "og:url",
-        content: "https://www.riinswork.space/",
-      },
-      {
-        property: "og:image",
-        content: "https://www.riinswork.space/static/ogpTitleLogo.png",
-      },
-      { property: "og:site_name", content: "Riin's Workspace" },
-      { property: "og:locale", content: "ja_JP" },
-      { name: "twitter:card", content: "summary_large_image" },
-      { name: "twitter:site", content: "@FruitRiin" },
-    ],
-    link: [],
-    style: [],
-    script: [],
-    noscript: [],
-  },
-  srcDir: 'src/',
+  srcDir: "src/",
   ssr: false,
-
+  app: {
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          charset: "utf-8",
+        },
+        {
+          property: "og:title",
+          content: "Riin's Workspace",
+        },
+        {
+          property: "og:description",
+          content: "Vueが好きな人",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:url",
+          content: "https://www.riinswork.space/",
+        },
+        {
+          property: "og:image",
+          content: "https://www.riinswork.space/static/ogpTitleLogo.png",
+        },
+        { property: "og:site_name", content: "Riin's Workspace" },
+        { property: "og:locale", content: "ja_JP" },
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:site", content: "@FruitRiin" },
+      ],
+      link: [],
+      style: [],
+      script: [],
+      noscript: [],
+    },
+  },
 
 });
